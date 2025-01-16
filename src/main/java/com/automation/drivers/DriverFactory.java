09c81ef4e49c7dbb023e00d4f5fe47a10583ffa0
mekaizen <nirupamghosh.blrin@gmail.com>
2025-01-16 12:42:34 +0530

    test

--- src/main/java/com/automation/drivers/DriverFactory.java
+++ src/main/java/com/automation/drivers/DriverFactory.java
@@ -3,28 +3,43 @@ package com.automation.drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class DriverFactory {
 
     public static WebDriver getDriver(String browser) {
         WebDriver driver = null;
-        
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
+        String remoteUrl = System.getProperty("remoteUrl");
+try {
+    switch (browser.toLowerCase()) {
+        case "chrome":
+            if (remoteUrl != null && !remoteUrl.isEmpty()) {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                driver = new RemoteWebDriver(new URL(remoteUrl), chromeOptions);
+            } else {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                driver = new ChromeDriver(chromeOptions);
+            }
+            break;
+        case "firefox":
 
-                driver = new FirefoxDriver();
-                break;
-            default:
-                System.out.println("Unsupported browser. Defaulting to Chrome.");
-                driver = new ChromeDriver();
-                break;
-        }
-        
+            driver = new FirefoxDriver();
+            break;
+        default:
+            System.out.println("Unsupported browser. Defaulting to Chrome.");
+            driver = new ChromeDriver();
+            break;
+    }
+}catch (Exception e){
+    e.printStackTrace();
+}
         return driver;
     }
 }
